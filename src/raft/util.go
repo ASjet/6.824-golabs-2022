9ebb653604f558ce3d5218afe93deb1b20ea9bdd
Aryan Sjet <Aryan.ITFS@Gmail.com>
2022-08-23 11:34:48 +0800

    feat(util): add log util

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,13 +1,64 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 // Debugging
-const Debug = false
+const (
+	DEBUG     = true
+	LOG_LEVEL = 1
+)
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug {
-		log.Printf(format, a...)
+var debugLogger *log.Logger
+var infoLogger *log.Logger
+var warningLogger *log.Logger
+
+func init() {
+	if LOG_LEVEL < 1 {
+		debugLogger = log.New(os.Stdout, "DEBUG ", log.Ltime|log.Lmicroseconds|log.Lmsgprefix)
+	} else {
+		odebug, err := os.Create("debug.log")
+		if err != nil {
+			log.Fatalf("init: %v", err)
+		}
+		debugLogger = log.New(odebug, "DEBUG ", log.Ltime|log.Lmicroseconds|log.Lmsgprefix)
 	}
-	return
+	if LOG_LEVEL < 2 {
+		infoLogger = log.New(os.Stdout, "INFO  ", log.Ltime|log.Lmicroseconds|log.Lmsgprefix)
+	} else {
+		oinfo, err := os.Create("info.log")
+		if err != nil {
+			log.Fatalf("init: %v", err)
+		}
+		infoLogger = log.New(oinfo, "INFO  ", log.Ltime|log.Lmicroseconds|log.Lmsgprefix)
+	}
+	if LOG_LEVEL < 3 {
+		warningLogger = log.New(os.Stderr, "WARN  ", log.Ltime|log.Lmicroseconds|log.Lmsgprefix)
+	} else {
+		owarn, err := os.Create("warning.log")
+		if err != nil {
+			log.Fatalf("init: %v", err)
+		}
+		warningLogger = log.New(owarn, "WARN  ", log.Ltime|log.Lmicroseconds|log.Lmsgprefix)
+	}
+}
+
+func debug(fmts string, args ...any) {
+	if DEBUG {
+		debugLogger.Printf(fmts, args...)
+	}
+}
+
+func info(fmts string, args ...any) {
+	if DEBUG {
+		infoLogger.Printf(fmts, args...)
+	}
+}
+
+func warning(fmts string, args ...any) {
+	if DEBUG {
+		warningLogger.Printf(fmts, args...)
+	}
 }
