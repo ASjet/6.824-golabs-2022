912eb25f2029c3bf5c0d7f2b52a485e2aff90911
Aryan Sjet <Aryan.ITFS@Gmail.com>
2022-08-27 17:08:13 +0800

    feat(log.py): add column parameter

--- src/raft/log.py
+++ src/raft/log.py
@@ -3,38 +3,41 @@ from rich.console import Console
 from rich.table import Table
 
 
-def parse(line: str) -> list[str]:
-    row = [None] * 4
+def parse(columns: int, line: str) -> list[str]:
+    row = [None] * (1 + columns)
     row[0] = line[16:21]
     num = int(line[23:24])
-    row[num + 1] = line[25:-1]
+    content = line[25:-1]
+    if num >= columns:
+        content = f"[{num}]{content}"
+    row[num % columns + 1] = content
     return row
 
 
-def print_table(file: str, title: str, lines: int = -1) -> None:
+def print_table(file: str, title: str, columns: int = 3, lines: int = -1) -> None:
     table = Table(title=title)
 
     table.add_column("LEVEL", justify="left", style="cyan", no_wrap=False)
-    table.add_column("[0]", no_wrap=False)
-    table.add_column("[1]", no_wrap=False)
-    table.add_column("[2]", no_wrap=False)
+    for i in range(columns):
+        table.add_column(f"[{i}]", no_wrap=False)
 
     with open(file, "r") as f:
         end_section = False
         first = True
         last_row = []
-        line_cnt = 0
-        for line in f.readlines():
+        for num, line in enumerate(f.readlines()):
+            if lines > 0 and num > lines:
+                break
             if first:
                 try:
-                    last_row = parse(line)
+                    last_row = parse(columns, line)
                     first = False
                 except:
                     if line[22:26] == "Make":
                         continue
             else:
                 try:
-                    row = parse(line)
+                    row = parse(columns, line)
                     table.add_row(*last_row, end_section=end_section)
                     last_row = row
                     end_section = False
@@ -42,9 +45,6 @@ def print_table(file: str, title: str, lines: int = -1) -> None:
                     if line[22:26] == "Make" and not end_section:
                         end_section = True
                     continue
-            if lines > 0 and line_cnt > lines:
-                break
-            line_cnt += 1
         table.add_row(*last_row)
 
     console = Console()
@@ -52,10 +52,17 @@ def print_table(file: str, title: str, lines: int = -1) -> None:
 
 
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print(f"Usage: {sys.argv[0]} <filename> [lines] [columns]")
+        exit(0)
     filename = sys.argv[1]
     try:
         linenum = int(sys.argv[2])
     except:
         linenum = -1
+    try:
+        columns = int(sys.argv[3])
+    except:
+        columns = 3
 
-    print_table(filename, "Raft Log", linenum)
+    print_table(filename, "Raft Log", columns, linenum)
